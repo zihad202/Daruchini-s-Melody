16ac6808e1f7d9bb7bf80fbedbbb8313f40a75d0
zihad202 <125727578+zihad202@users.noreply.github.com>
2026-09-12 13:30:28 +0600

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daruchini.melody',
+  appName: "Daruchini's Melody",
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
